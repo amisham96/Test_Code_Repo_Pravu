5fe87086a1e142df92038068c4efc7026ef35830
Amit Sharma <82436743+amisham96@users.noreply.github.com>
2025-01-21 19:04:18 +0530

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,16 +1,29 @@
 import azure.functions as func
-import datetime
-import json
 import logging
-import newconfig
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False,
-              use_monitor=False) 
-def timer_trigger(myTimer: func.TimerRequest) -> None:
-    
-    if myTimer.past_due:
-        logging.info('The timer is past due!')
+@app.timer_trigger(schedule="0 */1 * * * *", arg_name="myTimer", run_on_startup=True,
+                   use_monitor=False)
+def zsdbi(myTimer: func.TimerRequest) -> None:
 
-    logging.info('Python timer trigger function executed.')
+    try:
+        logging.info('36 Python timer trigger function executed.')
+        import zsbiconfig
+        import sftplib
+        # import triggerfunc
+
+        if myTimer.past_due:
+            logging.info('The timer is past due!')
+
+        config = zsbiconfig.MyConfig(azure=True)
+        print('config.ftp_host:', config.ftp_host)
+        print('config.ftp_username:', config.ftp_username)
+        print('config.ftp_password:', config.ftp_password)
+
+        # err = triggerfunc.triggerfunc(config)
+
+    except Exception as e:
+        logging.exception(e)
+
+    return
